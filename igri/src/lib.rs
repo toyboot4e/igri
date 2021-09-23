253c5090f1388c591c9006b2728ac14edc38913d
toyboot4e <toyboot4e@gmail.com>
2021-09-23 10:59:17 +0900

    doc attributes

--- igri/src/lib.rs
+++ igri/src/lib.rs
@@ -12,9 +12,24 @@ pub struct MyCoolStruct<T> {
 
 # `enum` support
 
-Default `enum` inspector is implemented as a tag selector + variant field inspectors. On the tag
-change, the inspected value is replaced with the target variant with default values. If any of the
-variant field does not satisfy the `Default` trait, the `Inspect` trait derivation fails.
+`#[derive(Inspect)]` for `enum` by default is implemented as a tag selector + variant field
+inspectors. On the tag change, the inspected value is replaced with the target variant with default
+values. If any of the variant field do not satisfy the `Default` trait, the `Inspect` trait
+derivation fails.
+
+If you specify `#[inspect(no_tag)]` attribute, `Inspect` is implemented without tag switcher, and
+the `Default` implementation is not needed.
+
+# Supported attributes (`#[inspect(attribute = value)]`)
+
+| attribute             | over          | description                                                                           |
+|---                    |---            |---                                                                                    |
+| `skip`                | type or field | Skip inspection                                                                       |
+| `with = "<function>"` | type or field | Override the inspection with the function                                             |
+| `as  = "<Type>"`      | type or field | Inspect as the type, converting with `Into`                                           |
+| `open = <bool>`       | type          | If the node is open by default                                                        |
+| `bounds = "<bounds>"` | type          | Override `where` boundary for the `Inspect` impl (default: each `FieldType: Inspect`) |
+| `no_tag`              | type (`enum`) | Disable tag selector (see `enum` support for more info)                               |
 
 # `dummy` feature
 
@@ -26,10 +41,10 @@ We want to disable developer UI on release build. Enable `dummy` feature flag to
 # Limitations
 
 `Inspect` is a foreign trait from your code, which can only be implemented for types in your own
-crate. So types upstream framework types do not implement `Inspect`.
+crate. So upstream framework types do not implement `Inspect`.
 
-`igri` lets you tweak values via `imgui`, but it doesn't let you propagate the change (so for
-example, your game view is not synced to changes made with `igri`).
+`igri` lets you tweak values via `imgui`, but it doesn't let you propagate the change. For example,
+your game view would not be synced to changes made with `igri`.
 */
 
 #[cfg(not(feature = "dummy"))]
